d46cb9dc6e2426995a64affc052889bc4e824f9b
Ari Susanto <risusanto@outlook.com>
2018-07-08 13:20:13 +0700

    test

---
+++ index.php
@@ -0,0 +1,19 @@
+$connectstr_dbhost = '';
+$connectstr_dbname = '';
+$connectstr_dbusername = '';
+$connectstr_dbpassword = '';
+
+foreach ($_SERVER as $key => $value) {
+    if (strpos($key, "MYSQLCONNSTR_localdb") !== 0) {
+        continue;
+    }
+    
+    $connectstr_dbhost = preg_replace("/^.*Data Source=(.+?);.*$/", "\\1", $value);
+    $connectstr_dbname = preg_replace("/^.*Database=(.+?);.*$/", "\\1", $value);
+    $connectstr_dbusername = preg_replace("/^.*User Id=(.+?);.*$/", "\\1", $value);
+    $connectstr_dbpassword = preg_replace("/^.*Password=(.+?)$/", "\\1", $value);
+}
+
+echo $connectstr_dbhost.<br>;
+echo $connectstr_dbusername.<br>;
+echo $connectstr_dbpassword.<br>;
